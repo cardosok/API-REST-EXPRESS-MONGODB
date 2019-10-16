aac0c1e72cdf0de49cafe86c364df2a5099cf87c
DNascto <dgn.nascto@gmail.com>
2019-10-16 01:17:48 -0300

    Adicionando a data atual para salvar no mongo

--- models/horta.js
+++ models/horta.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 const hortaSchemas = mongoose.Schema({
     umidadeDoSolo: String,
     temperaturaDoAr: Number,
-    umidade: Number
+    umidade: Number,
+    date: {
+        type: Date,
+        default:Date.now
+    }
 });
 
 module.exports = mongoose.model('hortaModel', hortaSchemas);
